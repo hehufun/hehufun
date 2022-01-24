d5a1fabe0df36cb095ce1b157bf61c5030197ec7
hehufun <hehu@hehu.fun>
2022-01-24 19:22:20 +0800

    1.0

--- js/main.js
+++ js/main.js
@@ -1,31 +1,37 @@
 /* global KEEP */
 
-window.addEventListener('DOMContentLoaded', () => {
-
+window.addEventListener("DOMContentLoaded", () => {
   KEEP.themeInfo = {
     theme: `Keep v${KEEP.theme_config.version}`,
-    author: 'XPoet',
-    repository: 'https://github.com/XPoet/hexo-theme-keep'
-  }
+    author: "XPoet",
+    repository: "https://github.com/XPoet/hexo-theme-keep",
+  };
 
-  KEEP.localStorageKey = 'KEEP-THEME-STATUS';
+  KEEP.localStorageKey = "KEEP-THEME-STATUS";
 
   KEEP.styleStatus = {
     isExpandPageWidth: false,
     isDark: false,
     fontSizeLevel: 0,
-    isOpenPageAside: true
-  }
+    isOpenPageAside: true,
+  };
 
   // print theme base info
   KEEP.printThemeInfo = () => {
-    console.log(`\n %c ${KEEP.themeInfo.theme} %c ${KEEP.themeInfo.repository} \n`, `color: #fadfa3; background: #333; padding: 5px 0;`, `background: #fadfa3; padding: 5px 0;`);
-  }
+    console.log(
+      `\n %c ${KEEP.themeInfo.theme} %c ${KEEP.themeInfo.repository} \n`,
+      `color: #fadfa3; background: #333; padding: 5px 0;`,
+      `background: #fadfa3; padding: 5px 0;`
+    );
+  };
 
   // set styleStatus to localStorage
   KEEP.setStyleStatus = () => {
-    localStorage.setItem(KEEP.localStorageKey, JSON.stringify(KEEP.styleStatus));
-  }
+    localStorage.setItem(
+      KEEP.localStorageKey,
+      JSON.stringify(KEEP.styleStatus)
+    );
+  };
 
   // get styleStatus from localStorage
   KEEP.getStyleStatus = () => {
@@ -39,7 +45,7 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
       return null;
     }
-  }
+  };
 
   KEEP.refresh = () => {
     KEEP.initUtils();
@@ -58,28 +64,8 @@ window.addEventListener('DOMContentLoaded', () => {
     if (KEEP.theme_config.lazyload.enable === true) {
       KEEP.initLazyLoad();
     }
-  }
+  };
 
   KEEP.printThemeInfo();
   KEEP.refresh();
-});
-
-// 添加动态标题
-var link =
-  document.querySelector('link[rel*="icon"]') || document.createElement("link");
-link.rel = "shortcut icon";
-
-document.addEventListener("visibilitychange", function () {
-  if (document.visibilityState == "hidden") {
-    normal_title = document.title;
-    document.title = "❌记得关掉❌";
-    link.href = "./images/logo1.svg";
-  } else {
-    document.title = "💓欢迎回来💓";
-    setTimeout(function () {
-      document.title = normal_title;
-    }, 1600);
-    link.href = "./images/logo.svg";
-  }
-  document.getElementsByTagName("head")[0].appendChild(link);
 });
